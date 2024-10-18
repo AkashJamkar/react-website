204bd89e43e0dc52968368992b7c3c2a21a07903
AkashJamkar <124022380+AkashJamkar@users.noreply.github.com>
2024-10-18 11:13:56 -0500

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Hero.css'
+import arrow_icon from '../Assets/arrow.png'
+import main_image from '../Assets/main_image.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+            <div className="hero-hand-icon">
+                <p>Make Your Fashion Look</p>
+            </div>
+            <p className='change'>Mire <span className='color'>Charming</span></p>
+        </div>
+        <div className="hero-latest-btn">
+            <div>Latest Collection</div>
+            <img src={arrow_icon} alt="" />
+        </div>
+        </div>
+        <div className="hero-right">
+            <img src={main_image} alt="" />
+        </div>
+    
+        
+
+    </div>
+  )
+}
+
+export default Hero
